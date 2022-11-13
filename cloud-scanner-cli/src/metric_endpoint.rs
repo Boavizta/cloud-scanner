377b334da5ee1b3261b09afac2834875266b8cc6
olivier de Meringo <demeringo@gmail.com>
2022-11-13 13:21:07 +0100

    add region query string to metrics server

--- cloud-scanner-cli/src/metric_endpoint.rs
+++ cloud-scanner-cli/src/metric_endpoint.rs
@@ -16,14 +16,14 @@ pub async fn run() -> Result<(), rocket::Error> {
 
 #[get("/")]
 fn index() -> &'static str {
-    "Cloud scanner metric endpoint is up. Values are exposed at /metrics"
+    "Cloud scanner metric endpoint is up.\nValues are exposed at /metrics nd require passing a region e.g.  http://localhost:8000/metrics?aws_region=eu-west-3"
 }
 
-#[get("/metrics")]
-async fn metrics() -> String {
+#[get("/metrics?<aws_region>")]
+async fn metrics(aws_region: &str) -> String {
     let hours_use_time: f32 = 1.0;
     let tags = Vec::new();
-    let aws_region = "eu-west-1".to_string();
+    //let aws_region = "eu-west-1".to_string();
     let api_url = "https://api.boavizta.org";
     let metrics =
         crate::get_default_impacts_as_metrics(&hours_use_time, &tags, &aws_region, &api_url).await;
